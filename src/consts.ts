aaf87047812d240cbb7f2bd8a85074a03b2e7336
Thejan Vithanage <thejanvithanage@outlook.com>
2025-01-19 11:30:19 +0530

    v1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Connect | Thejan’s Digital Space';
+export const SITE_DESCRIPTION = 'Connect with Thejan Vithanage – find all my contact details in one place. Whether you want to collaborate, network, or just say hello, you’re in the right spot!';
